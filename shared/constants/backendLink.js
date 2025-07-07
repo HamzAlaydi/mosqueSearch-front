5ceefa6ec1fcb3676edb9c02a2b9375d2209b803
hamza <hamza.alaydi.99@gmail.com>
2025-07-07 04:02:28 +0300

    t1

--- shared/constants/backendLink.js
+++ shared/constants/backendLink.js
@@ -1,6 +1,8 @@
 // shared/constants/backendLink.js
-export const rootRoute = process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000/api";
+export const rootRoute = "http://18.170.117.67/api";
 export const backendLink =
-  process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000/api";
+  process.env.NEXT_PUBLIC_API_URL || "http://18.170.117.67/api";
 export const GOOGLE_API = "AIzaSyDWxhNp15n52e7kPQkfTTumYZT9E20cMHg";
 export const FLAG_KEY = "xi13wHV/r789FLVgaTROaQ==ErfchmfqYA2YDF8i";
+
+//
