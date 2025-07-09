8ad17c2a3924958f28300afbc2ca07eda1cf95d3
hamza <hamza.alaydi.99@gmail.com>
2025-07-09 16:00:10 +0300

    change the backend link

--- shared/constants/backendLink.js
+++ shared/constants/backendLink.js
@@ -1,7 +1,7 @@
 // shared/constants/backendLink.js
-export const rootRoute = "http://mosquematch.com/api";
+export const rootRoute = "https://mosquematch.com/api";
 export const backendLink =
-  process.env.NEXT_PUBLIC_API_URL || "http://mosquematch.com/api";
+  process.env.NEXT_PUBLIC_API_URL || "https://mosquematch.com/api";
 export const GOOGLE_API = "AIzaSyDWxhNp15n52e7kPQkfTTumYZT9E20cMHg";
 export const FLAG_KEY = "xi13wHV/r789FLVgaTROaQ==ErfchmfqYA2YDF8i";
 
