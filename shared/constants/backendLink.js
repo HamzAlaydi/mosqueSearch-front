fc69bd1f2a9686fc128d9382134e297c187bbe27
hamza <hamza.alaydi.99@gmail.com>
2025-07-24 07:01:20 +0300

    the link

--- shared/constants/backendLink.js
+++ shared/constants/backendLink.js
@@ -1,12 +1,7 @@
 // shared/constants/backendLink.js
-// export const rootRoute = "https://mosquematch.com/api";
-// export const backendLink =
-//   process.env.NEXT_PUBLIC_API_URL || "https://mosquematch.com/api";
-export const rootRoute = "http://localhost:5000/api";
+export const rootRoute = "https://mosquematch.com/api";
 export const backendLink =
-  process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000/api";
-
-
+  process.env.NEXT_PUBLIC_API_URL || "https://mosquematch.com/api";
 export const GOOGLE_API = "AIzaSyDWxhNp15n52e7kPQkfTTumYZT9E20cMHg";
 export const FLAG_KEY = "xi13wHV/r789FLVgaTROaQ==ErfchmfqYA2YDF8i";
 
